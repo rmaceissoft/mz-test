cb0c624438609a835d5f71a75ff87b724f82d5cb
Reiner Marquez <rmaceissoft@gmail.com>
2015-11-08 22:42:09 -0500

    flake8 cleanup

--- mozio_test/urls.py
+++ mozio_test/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
